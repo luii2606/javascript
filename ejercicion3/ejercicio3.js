aac530ebcb382e657684b10d6774246064a69d57
“luii2606” <valemidx@gmail.com>
2024-12-06 11:40:57 -0500

    nadita3

---
+++ ejercicion3/ejercicio3.js
@@ -0,0 +1,10 @@
+// 3. Escribir un programa que pida al usuario dos números y muestre por pantalla su 
+// división, si el divisor es cero el programa debe mostrar un error, se debe manejar
+// mediante excepciones y el mensaje debe ser personalizado.
+
+let numero1 = parseInt(prompt("ingrese el primer numero"));
+let numero2= parseInt(prompt("ingrese el primer numero"));
+
+function dividir(){
+    
+}
